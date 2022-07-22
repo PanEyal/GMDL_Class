0cbe1d21962a5418ee9d190c1390dc9bb1eb153b
penne8 <paney@post.bgu.ac.il>
2022-07-22 15:32:09 +0300

    PCA complete

--- Assignment-6/pca.py
+++ Assignment-6/pca.py
@@ -1,14 +1,25 @@
 # Import Libraries
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 import utils
 
 
-def PCA(X,components):
-    pass
-    # TODO : write your code here
+def PCA(X, components):
+    scalar = StandardScaler(with_std=False)
+    X_normd = scalar.fit_transform(X)
+    correlation = np.cov(X_normd.transpose())
+    eigenvalues, eigenvectors = np.linalg.eigh(correlation)
+    p_components = eigenvectors[:, eigenvalues.shape[0] - components:]
+    p_components[:, [0, 1]] = p_components[:, [1, 0]]
+    return np.matmul(X, p_components).transpose()
+
 
 def main():
-    pass
-    # TODO : write your code here
+    X, Y = utils.load_MNIST()
+    X = X.reshape((X.shape[0], X.shape[1] * X.shape[2]))
+    tdr_X = PCA(X, 2)
+    utils.scatter_plot(tdr_X, Y, 10)
+
 
 if __name__ == "__main__":
     main()
