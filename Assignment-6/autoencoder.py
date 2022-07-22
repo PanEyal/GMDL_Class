5d05bbbf08ef2d42ab13bf3295dc0e7b326e09a8
penne8 <paney@post.bgu.ac.il>
2022-07-22 16:36:06 +0300

    CE 4

--- Assignment-6/autoencoder.py
+++ Assignment-6/autoencoder.py
@@ -6,15 +6,43 @@ from sklearn.preprocessing import StandardScaler
 import utils
 
 
-
 class autoencoder(nn.Module):
     def __init__(self):
         super(autoencoder, self).__init__()
-        # TODO : write your code here
+        self.name = "autoencoder"
 
-    def forward(self, x):
-        # TODO : write your code here
-        pass
+        # Encoder: affine function
+        self.encode = torch.nn.Sequential(
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 8),
+            nn.ReLU(),
+            nn.Linear(8, 4),
+            nn.ReLU(),
+            nn.Linear(4, 2),
+            nn.ReLU()
+        )
+        # Decoder: affine function
+        self.decode = torch.nn.Sequential(
+            nn.Linear(2, 4),
+            nn.ReLU(),
+            nn.Linear(4, 8),
+            nn.ReLU(),
+            nn.Linear(8, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 28 * 28),
+            nn.ReLU()
+        )
+
+    def forward(self, X):
+        out = self.encode(X)
+        out = self.decode(out)
+
+        return out
 
 
 class linear_autoencoder(nn.Module):
@@ -28,6 +56,31 @@ class linear_autoencoder(nn.Module):
 
 
 def main():
+    X, Y = utils.load_MNIST()
+    scalar = StandardScaler(with_std=False)
+    X = scalar.fit_transform(X)
+    X = torch.tensor(X, dtype=torch.float32)
+    Y = torch.tensor(Y, dtype=torch.float32)
+    train_dataset = torch.utils.data.TensorDataset(X, Y)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32)
+
+    # Computer Exercise 4
+    model = autoencoder()
+    criterion = nn.MSELoss()
+    optimaizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    autoencoder_losses = utils.train(30, train_loader, model, criterion, optimaizer)
+    embeddings, labels = utils.get_embedding(model, train_loader)
+    utils.scatter_plot(embeddings, labels, 10)
+
+    # Computer Exercise 5
+    model = linear_autoencoder()
+    criterion = nn.MSELoss()
+    optimaizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    linear_autoencoder_losses = utils.train(30, train_loader, model, criterion, optimaizer)
+    embeddings, labels = utils.get_embedding(model, train_loader)
+    utils.scatter_plot(embeddings, labels, 10)
+
+    utils.losses_plot(autoencoder_losses, linear_autoencoder_losses)
     # TODO : write your code here (import MNIST data, set dataloaders, train, etc)
     pass
 
